848001af53ff4554524ddfda1bd5420af7e6fff6
Phéna Proxima <djphenaproxima@gmail.com>
2014-11-20 11:44:25 -0500

    Documented all of InfiniteCollection's methods.

--- Backbone.InfiniteCollection.js
+++ Backbone.InfiniteCollection.js
@@ -9,6 +9,10 @@ Backbone.InfiniteCollection = Backbone.Collection.extend({
     this.on('reset', function() { this._index = 0; }, this);
   },
 
+  /**
+   * Returns the current page from left to right, wrapping around to the
+   * beginning if needed.
+   */
   getPage: function() {
     var slice = this.slice(this._index, this._index + this.pageSize);
     if (slice.length < this.pageSize) {
@@ -17,6 +21,9 @@ Backbone.InfiniteCollection = Backbone.Collection.extend({
     return new Backbone.Collection(slice);
   },
 
+  /**
+   * Retreats the index by pageSize, wrapping around to the end if needed.
+   */
   retreat: function() {
     this._index -= this.pageSize;
     if (this._index < 0) {
@@ -25,16 +32,25 @@ Backbone.InfiniteCollection = Backbone.Collection.extend({
     return this;
   },
 
+  /**
+   * Returns the previous page *without* retreating the index.
+   */
   behind: function() {
     var slice = this.retreat().getPage();
     this.advance();
     return slice;
   },
   
+  /**
+   * Returns the previous page, retreating the index.
+   */
   previous: function() {
     return this.retreat().getPage();
   },
 
+  /**
+   * Advances the index by pageSize, wrapping around to the beginning if needed.
+   */
   advance: function() {
     this._index += this.pageSize;
     if (this._index >= this.length) {
@@ -43,12 +59,19 @@ Backbone.InfiniteCollection = Backbone.Collection.extend({
     return this;
   },
 
+  /**
+   * Returns the next page *without* advancing the index.
+   */
   ahead: function() {
     var slice = this.advance().getPage();
     this.retreat();
     return slice;
   },
 
+  /**
+   * Returns the next page, advancing the index. This is the page *after* the
+   * current page (that's what getPage() is for).
+   */
   next: function() {
     return this.advance().getPage();
   }
