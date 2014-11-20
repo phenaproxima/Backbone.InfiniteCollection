2f4b03d420e4546220fb3f376577835e4f3acb75
Phéna Proxima <djphenaproxima@gmail.com>
2014-11-20 09:11:51 -0500

    Deleted a lot of extraneous white space.

--- Backbone.InfiniteCollection.js
+++ Backbone.InfiniteCollection.js
@@ -6,53 +6,42 @@ Backbone.InfiniteCollection = Backbone.Collection.extend({
 
   initialize: function(undefined, options) {
     _.extend(this, options);
-
     this.on('reset', function() { this._index = 0; }, this);
   },
 
   getPage: function() {
     var slice = this.slice(this._index, this._index + this.pageSize);
-
     if (slice.length < this.pageSize) {
       slice = slice.concat(this.slice(0, this.pageSize - slice.length));
     }
-
     return new Backbone.Collection(slice);
   },
 
   goBackward: function() {
     this._index -= this.pageSize;
-
     if (this._index < 0) {
       this._index += this.length;
     }
-
     return this;
   },
 
   lookBehind: function() {
     var slice = this.goBackward().getPage();
-
     this.goForward();
-
     return slice;
   },
 
   goForward: function() {
     this._index += this.pageSize;
-    
     if (this._index >= this.length) {
       this._index -= this.length;
     }
-
     return this;
   },
 
   lookAhead: function() {
     var slice = this.goForward().getPage();
-
     this.goBackward();
-
     return slice;
   }
 
